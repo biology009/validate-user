e6cbe72d7596cddf348aafab48d5941c575dbd2f
biology009 <bipotitanic@proton.me>
2026-06-25 10:44:01 +0530

    Update store-token.js

--- api/store-token.js
+++ api/store-token.js
@@ -14,7 +14,10 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
+  //const { url, userId, username } = req.body;
   const { url, userId, username } = req.body;
+  console.log(req.body);
+  console.log({ url, userId, username });
 
   if (!url) {
     return res.status(400).json({ error: "Missing URL" });
